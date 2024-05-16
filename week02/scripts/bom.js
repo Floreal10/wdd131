1d7da8106229b433dc067b53d0795f62bc8776bc
Floreal10 <floreal.a.ch@gmail.com>
2024-05-16 12:47:18 -0400

    .

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -6,7 +6,23 @@ const li = document.createElement('li');
 const deleteButton = document.createElement('button');
 
 li.textContent = input.value;
-deleteButton.textContent = 'X';
+deleteButton.textContent = '❌';
 
 li.append(deleteButton);
 list.append(li);
+
+button.addEventListener('click', function() {
+    // Code to execute when the button is clicked
+});
+
+if (input.value.trim() !== 'blank') {}
+
+
+
+deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.focus();
+});
+
+input.value = '';
+input.focus();
