90461218200d7c92334ee9494cf261991ce9e7d8
Floreal10 <floreal.a.ch@gmail.com>
2024-05-18 18:59:16 -0400

    .

--- index.html/scripts/temples.js
+++ index.html/scripts/temples.js
@@ -5,3 +5,11 @@ const lastModified = document.getElementById('lastModified');
 currentYear.textContent = new Date().getFullYear();
 
 lastModified.textContent = `Last modification: ${document.lastModified}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
