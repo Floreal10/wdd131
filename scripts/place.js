f2e5fbf9f7ea8f7439f803d384a6346ce9b6fb0b
Floreal10 <floreal.a.ch@gmail.com>
2024-05-25 22:46:46 -0400

    .

--- scripts/place.js
+++ scripts/place.js
@@ -5,3 +5,31 @@ const lastModified = document.getElementById('lastModified');
 currentYear.textContent = new Date().getFullYear();
 
 lastModified.textContent = `Last modification: ${document.lastModified}`;
+
+
+function calculateWindChill (temp, windSpeed) {
+
+    if (temp <=50 && windSpeed >3) {
+        return 35.74 + (0.6215 * temp) - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temp * Math.pow(windSpeed, 0.16);
+
+    } else {
+        return "N/A";
+    }
+}
+
+const windChill = document.getElementById('windchill');
+
+const temp = 48;
+const windSpeed = 4;
+
+
+const windChillResult = calculateWindChill(temp,windSpeed);
+
+windChill.textContent = windChillResult; 
+
+
+
+
+
+
+
